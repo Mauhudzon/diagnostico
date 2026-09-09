bec6413f6e5ab064e28db826469acef882ecb3ea
Kael <mauhudzon144@gmail.com>
2026-09-08 19:12:41 -0500

    Tercer Commit: Solucion de error de la Calculadora

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,11 +1,13 @@
-    let suma = 0;
+    function calcularPromedio(numeros) {
+        let suma = 0;
 
-    for (let i = 0; i <= numeros.length; i++) {
-        suma += numeros[i];
+        for (let i = 0; i < numeros.length; i++) {
+            suma += numeros[i];
+        }
+
+        return suma / numeros.length;
     }
-    return suma / numeros.length;
-}
 
-const valores = [10, 20, 30, 40];
+    const valores = [10, 20, 30, 40];
 
 console.log("Promedio:", calcularPromedio(valores));
