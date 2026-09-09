364be7d90b135af885e57e89e75394a84cd4c7a0
Kael <mauhudzon144@gmail.com>
2026-09-08 19:04:29 -0500

    Segundo Commit: Codigo de la Calculadora

---
+++ src/calculadora.js
@@ -0,0 +1,11 @@
+    let suma = 0;
+
+    for (let i = 0; i <= numeros.length; i++) {
+        suma += numeros[i];
+    }
+    return suma / numeros.length;
+}
+
+const valores = [10, 20, 30, 40];
+
+console.log("Promedio:", calcularPromedio(valores));
